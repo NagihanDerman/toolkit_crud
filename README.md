bd978253b74603b202cc3c07697dd301336a1efa
NagihanDerman <dermannagihan@gmail.com>
2024-07-23 19:48:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Redux_Toolkit_Crud
+
+Ce projet est une application de tableau de suivi des tâches. L'application permet aux utilisateurs d'ajouter et de supprimer des tâches et d'attribuer des tâches à des personnes spécifiques. Le projet a été développé en utilisant React, React Bootstrap, UUID, React Router DOM, React Icons et Redux Toolkit.
+
+## Fonctionnalités
+
+- Ajout de Tâches : Les utilisateurs peuvent ajouter de nouvelles tâches.
+- Suppression de Tâches : Les utilisateurs peuvent supprimer des tâches existantes.
+
+## Technologies et Bibliothèques Utilisées
+
+- React : Pour créer l'interface utilisateur.
+- React Bootstrap : Pour créer des interfaces utilisateurs modernes et réactives.
+- UUID : Pour générer des identifiants uniques.
+- React Router DOM : Pour la navigation au sein de l'application.
+- React Icons : Pour utiliser des icônes dans l'interface utilisateur.
+- Redux Toolkit : Pour la gestion de l'état de l'application.
+
+### GIF
+
+<img src=""/>
---
+++ src/redux/slices/counterSlice.js
@@ -0,0 +1,43 @@
+/*
+ * createSlice methodu import edilir
+ * createSlice'in istedği parametreler
+ * - - name: slice ismi > string
+ * - - initialState: başlangıç state'i
+ * - - reducers: aksiyonların görevlerini tanımladığımız fonksiyonlar
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0, isDarkTheme: true },
+  // state'in nasıl değişiceğine karar veren fonksiyonları tanımalama
+  reducers: {
+    // 1.aksiyon
+    increase: (state) => {
+      state.count++;
+    },
+
+    // 2.aksiyon
+    decrease: (state) => {
+      state.count--;
+    },
+
+    // 3.aksiyon
+    setCount: (state, action) => {
+      state.count = action.payload;
+    },
+
+    // 4.aksiyon
+    toggleTheme: (state) => {
+      state.isDarkTheme = !state.isDarkTheme;
+    },
+  },
+});
+
+// slice'İn oluşturduğu reducer fonksiyonunu store'a tanıtmak için export edelim
+export default counterSlice.reducer;
+
+// sliice'in oluşturduğu aksiyon oluşturan fonksiyonları bileşenlerde kullanabilmek için export edelim
+export const { decrease, increase, setCount, toggleTheme } =
+  counterSlice.actions;
